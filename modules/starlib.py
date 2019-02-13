cdd14af1e07388d663304e14aba669dae74ca69f
Unknown <Josrodjr@gmail.com>
2019-02-13 14:47:20 -0600

    This is madness!
    C-3PO

--- modules/starlib.py
+++ modules/starlib.py
@@ -1,36 +1,40 @@
-#library for some operations
+# library for some operations
+
 
 def check_horizontal(h_array):
-  for i in range(len(h_array)):
-    for j in range(len(h_array)):
-      if (i != j):
-        if h_array[i] == h_array[j]:
-          return 1
-  return 0
+    for i in range(len(h_array)):
+        for j in range(len(h_array)):
+            if (i != j):
+                if h_array[i] == h_array[j]:
+                    return 1
+    return 0
 
-def check_vertical(a,b,c,d):
-  v_array = [a,b,c,d]
-  for i in range(len(v_array)):
-    for j in range(len(v_array)):
-      if (i != j):
-        if v_array[i] == v_array[j]:
-          return 1
-  return 0
 
-def check_win(current_state):
-  #check verticals
-  for i in range(len(current_state)):
-    victory = check_vertical(current_state[0][i], current_state[1][i], current_state[2][i], current_state[3][i])
+def check_vertical(a, b, c, d):
+    v_array = [a, b, c, d]
+    for i in range(len(v_array)):
+        for j in range(len(v_array)):
+            if (i != j):
+                if v_array[i] == v_array[j]:
+                    return 1
+    return 0
+
+
+def check_win(cs):
+    # check verticals
+    for i in range(len(cs)):
+        victory = check_vertical(cs[0][i], cs[1][i], cs[2][i], cs[3][i])
+        if victory == 1:
+            return 1
+
+# check horizontals
+    for i in range(len(cs)):
+        victory = check_horizontal(cs[i])
     if victory == 1:
-      return 1
-  #check horizontals
-  for i in range(len(current_state)):
-    victory = check_horizontal(current_state[i])
-    if victory == 1:
-      return 1
-  #best case victory
-  return 0
+        return 1
+# best case victory
+    return 0
 
-#tests for the methods
-# print(check_horizontal([5,2,1,4]))
-# print(check_vertical(1,4,3,4))
+# tests for the methods
+# print(check_horizontal([5, 2, 5, 4]))
+# print(check_vertical(1, 2, 3, 4))
--- sudoku.py
+++ sudoku.py
@@ -1,24 +1,24 @@
-#python sudoku.py start=.4.13.4.1..4.21.
+# python sudoku.py start=.4.13.4.1..4.21.
 
 import sys
 
-#get the first arg and parse out the start
+# get the first arg and parse out the start
 
 parsed_string = ''
-#generate the matrix where we will store the starting state
+# generate the matrix where we will store the starting state
 init_matrix = []
 
 if str(sys.argv[1]):
-  full_string = str(sys.argv[1])
-  parsed_string = full_string.replace('start=','')
+    full_string = str(sys.argv[1])
+    parsed_string = full_string.replace('start=', '')
 
-#parse the empty values located inside the sudoku
+# parse the empty values located inside the sudoku
 temporal_arr = []
 for i in range(len(parsed_string)):
-  j = i+1
-  temporal_arr.append(parsed_string[i])
-  if (j % 4 == 0):
-    init_matrix.append(temporal_arr)
-    temporal_arr = []
+    j = i+1
+    temporal_arr.append(parsed_string[i])
+    if (j % 4 == 0):
+        init_matrix.append(temporal_arr)
+        temporal_arr = []
 
 print(init_matrix[0])
